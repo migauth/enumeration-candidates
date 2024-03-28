27d371dd5a300ccc0a36d4587b403358aa71ec53
Michael Gauthier <mikegauthier222@hotmail.com>
2024-03-28 15:25:53 -0700

    define methods

--- filters.rb
+++ filters.rb
@@ -2,15 +2,42 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+    # Your code Here 
+
+    @candidates.find { |candidate| candidate[:id] == id}
+
+end
   
-  def experienced?(candidate)
+def experienced?(candidate)
     # Your code Here
-  end
+    candidate[:years_of_experience] >= 2
+end
   
-  def qualified_candidates(candidates)
+
+def knows_ruby_and_python?(candidate)
+  candidate[:languages].include?('Ruby') && candidate[:languages].include?('Python')
+end
+
+def applied_within_15?(candidate)
+  # Your code Here
+  (0.days.ago.to_date.day - candidate[:date_applied].day) <= 15
+end
+
+def over_17?(candidate)
+  # Your code Here
+  candidate[:age] >= 18
+end
+  
+def qualified_candidates(candidates)
     # Your code Here
-  end
+    candidates.select do |candidate|
+      experienced?(candidate) && candidate[:github_points] >= 100 && knows_ruby_and_python?(candidate) && applied_within_15?(candidate) && over_17?(candidate)
+    end
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
+
   
   # More methods will go below
--- main.rb
+++ main.rb
@@ -6,4 +6,22 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates
+
+# @candidates.each do |candidate|
+#   puts "Candidate ID #{candidate[:id]} is experienced? #{experienced?(candidate)}"
+# end
+
+# puts "Here is the canidiate: #{find(5)}"
+
+# puts qualified_candidates
+
+# qualified = qualified_candidates(@candidates)
+# puts "Qualified candidates:"
+# puts qualified
+
+# @candidates.each do |candidate|
+#   puts "#{applied_within_15?(candidate)}"
+# end
+
+puts ordered_by_qualifications(@candidates)
